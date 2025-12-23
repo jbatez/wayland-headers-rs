17c52f72cc580302ba1beedb9a554bd9d9323632
Jo Bates <29763794+jbatez@users.noreply.github.com>
2025-12-23 08:39:01 -0800

    update WAYLAND_VERSION_MINOR to 24

--- wayland-headers/src/wayland_version.rs
+++ wayland-headers/src/wayland_version.rs
@@ -1,6 +1,6 @@
 use core::ffi::{CStr, c_int};
 
 pub const WAYLAND_VERSION_MAJOR: c_int = 1;
-pub const WAYLAND_VERSION_MINOR: c_int = 18;
+pub const WAYLAND_VERSION_MINOR: c_int = 24;
 pub const WAYLAND_VERSION_MICRO: c_int = 0;
-pub const WAYLAND_VERSION: &CStr = c"1.18.0";
+pub const WAYLAND_VERSION: &CStr = c"1.24.0";
